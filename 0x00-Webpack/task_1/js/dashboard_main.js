4cf5dd4f990a57c81e08d175c6dce49720db2d71
samfrexz619 <samfrexz69@gmail.com>
2024-01-02 21:08:36 +0100

    add files

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,1 +1,21 @@
-const $ = require('jquery')
+import $ from 'jquery'
+import debounce from 'lodash/debounce';
+
+function addElements() {
+  $('<p>').text('Holberton Dashboard').appendTo('body');
+  $('<p>').text('Dashboard data for the students').appendTo('body');
+  $('<button>').text('Click here to get started').appendTo('body');
+  $('<p>').attr('id', 'count').appendTo('body');
+  $('<p>').text('Copyright - Holberton School').appendTo('body');
+}
+
+function updateCounter() {
+  let counter = parseInt($('#count').text()) || 0;
+  counter++;
+  $('#count').text(`${counter} clicks on the button`);
+}
+
+$(function () {
+  addElements();
+  $('button').on('click', debounce(updateCounter, 100));
+});
