7d35d971c622c8712c71b12b717f7f6dc7b5ae2e
samfrexz619 <samfrexz69@gmail.com>
2024-01-02 21:29:48 +0100

    upd

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,9 +1,11 @@
 const $ = require('jquery');
 
-$(function (){
-  const para1 = $("<p>Holberton Dashboard</p>");
-  const para2 = $("<p>Dashboard data for the students</p>");
-  const para3 = $("<p>Copyright - Holberton School</p>");
-  
-  $("body").append(para1, para2, para3)
+function addParagraphs () {
+  $('<p>').text('Holberton Dashboard').appendTo('body');
+  $('<p>').text('Dashboard data for the students').appendTo('body');
+  $('<p>').text('Copyright - Holberton School').appendTo('body');
+};
+
+$(function () {
+  addParagraphs();
 });
--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,7 +1,7 @@
-import $ from 'jquery'
+import $ from 'jquery';
 import debounce from 'lodash/debounce';
 
-function addElements() {
+function addElements () {
   $('<p>').text('Holberton Dashboard').appendTo('body');
   $('<p>').text('Dashboard data for the students').appendTo('body');
   $('<button>').text('Click here to get started').appendTo('body');
@@ -9,8 +9,8 @@ function addElements() {
   $('<p>').text('Copyright - Holberton School').appendTo('body');
 }
 
-function updateCounter() {
-  let counter = parseInt($('#count').text()) || 0;
+function updateCounter () {
+  const counter = parseInt($('#count').text()) || 0;
   counter++;
   $('#count').text(`${counter} clicks on the button`);
 }
