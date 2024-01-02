89b977442b185563ef55177dbe19b91fa06d0cdb
samfrexz619 <samfrexz69@gmail.com>
2024-01-02 16:46:46 +0100

    add new dir

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,10 +1,9 @@
-const $ = require('jquery')
+const $ = require('jquery');
 
 $(document).ready(function (){
-  const para1 = $("<p>Holberton Dashboard</p>")
-  const para2 = $("<p>Dashboard data for the students</p>")
-  const para3 = $("<p>Copyright - Holberton School</p>")
+  const para1 = $("<p>Holberton Dashboard</p>");
+  const para2 = $("<p>Dashboard data for the students</p>");
+  const para3 = $("<p>Copyright - Holberton School</p>");
   
   $("body").append(para1, para2, para3)
-  // $("body").append(para2)
-})
+});
