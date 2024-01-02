6651db64cc84091080a97a716d51fc6c5357ef73
samfrexz619 <samfrexz69@gmail.com>
2024-01-02 16:10:27 +0100

    add files

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,10 @@
+const $ = require('jquery')
+
+$(document).ready(function (){
+  const para1 = $("<p>Holberton Dashboard</p>")
+  const para2 = $("<p>Dashboard data for the students</p>")
+  const para3 = $("<p>Copyright - Holberton School</p>")
+  
+  $("body").append(para1, para2, para3)
+  // $("body").append(para2)
+})
