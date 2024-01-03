f47f0347fe37a020b87eb881540b0caccf21a6f6
samfrexz619 <samfrexz69@gmail.com>
2024-01-03 09:40:27 +0100

    upd js file

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,9 +1,7 @@
 const $ = require('jquery');
 
 $(function(){
-  let para1 = $("<p>Holberton Dashboard</p>");
-  let para2 = $("<p>Dashboard data for the students</p>");
-  let para3 = $("<p>Copyright - Holberton School</p>");
-
-  $("body").append(para1, para2, para3);
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
 });
