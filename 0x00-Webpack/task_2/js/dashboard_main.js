c0d236c4899e59be9e16041a7a825d9861cef85c
samfrexz619 <samfrexz69@gmail.com>
2024-01-03 15:36:58 +0100

    add files

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,14 +1,8 @@
 import $ from 'jquery';
-import debounce from 'lodash/debounce';
-// import '../css/main.css'
+import _ from 'lodash';
+import logo from '../assets/holberton-logo.jpg'
+import '../css/main.css'
 
-function addElements () {
-  $('<p>').text('Holberton Dashboard').appendTo('body');
-  $('<p>').text('Dashboard data for the students').appendTo('body');
-  $('<button>').text('Click here to get started').appendTo('body');
-  $('<p>').attr('id', 'count').appendTo('body');
-  $('<p>').text('Copyright - Holberton School').appendTo('body');
-};
 
 function updateCounter () {
   let counter = parseInt($('#count').text()) || 0;
@@ -16,7 +10,15 @@ function updateCounter () {
   $('#count').text(`${counter} clicks on the button`);
 };
 
+let debounce = _.debounce(updateCounter, 500);
+
 $(function () {
-  addElements();
-  $('button').on('click', debounce(updateCounter, 100));
+  $('<div>').attr('id', 'logo').css('background-image', `url(${logo})`).appendTo('body');
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button>Click here to get started</button>');
+  $('<p>').attr('id', 'count').appendTo('body');
+  $('body').append('<p>Copyright - Holberton School</p>');
+
+  $('button').on('click', debounce);
 });
--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -21,6 +21,30 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75
+              }
+            }
+          }
+        ]
       }
     ]
   },
