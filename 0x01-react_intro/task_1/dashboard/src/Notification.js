e1c4751b36177c87f32275d073b4aa9c974d8592
samfrexz619 <samfrexz69@gmail.com>
2024-01-04 08:13:49 +0100

    add notification component

---
+++ 0x01-react_intro/task_1/dashboard/src/Notification.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Notifications.css'
+
+
+const Notification = () => {
+  return (
+    <div className='Notifications'>
+      <p>Here is the list of notifications</p>
+    </div>
+  )
+}
+
+export default Notification;
--- 0x01-react_intro/task_1/dashboard/src/index.js
+++ 0x01-react_intro/task_1/dashboard/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Notification from './Notification';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <div className='root-notifications'>
+      <Notification  />
+    </div>
   </React.StrictMode>
 );
 
