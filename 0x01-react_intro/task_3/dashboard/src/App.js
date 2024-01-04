311a7b90122e8a1f9fee00e998d683767ec0e833
samfrexz619 <samfrexz69@gmail.com>
2024-01-04 17:26:08 +0100

    add test notification

--- 0x01-react_intro/task_3/dashboard/src/App.js
+++ 0x01-react_intro/task_3/dashboard/src/App.js
@@ -6,10 +6,10 @@ import { getFooterCopy, getFullYear } from './utils';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} alt="logo" />
         <h1>School dashboard</h1>
-      </header>
+      </div>
       <div className='App-body'>
         <div>
           <p>Login to access the full dashboard</p>
@@ -22,11 +22,9 @@ function App() {
           </button>
         </div>
       </div>
-      <footer>
-        <div className="App-footer">
-          <p><i>Copyright { getFullYear()} - {getFooterCopy(true)}</i></p>
-        </div>
-      </footer>
+      <div className="App-footer">
+        <p><i>Copyright { getFullYear()} - {getFooterCopy(true)}</i></p>
+      </div>
     </div>
   );
 };
