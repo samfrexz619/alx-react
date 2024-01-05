a49c0b18bf1437c57b801504705799e94d5d44dc
samfrexz619 <samfrexz69@gmail.com>
2024-01-05 21:04:57 +0100

    upd file

--- 0x00-Webpack/task_3/modules/body/body.js
+++ 0x00-Webpack/task_3/modules/body/body.js
@@ -2,22 +2,18 @@ import $ from 'jquery';
 import _ from 'lodash';
 import './body.css';
 
+$('body').append('<div id="logo"></div>');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
-function updateCounter () {
-  let counter = parseInt($('#count').text()) || 0;
+let counter = 0;
+
+function updateCounter() {
   counter++;
-  $('#count').text(`${counter} clicks on the button`);
+  $("#count").html(`${counter} clicks on the button`);
 };
 
-let debounce = _.debounce(updateCounter, 500);
-
-$(function () {
-  $('body').append('<div id="logo"></div>');  
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button>Click here to get started</button>');
-  $('<p>').attr('id', 'count').appendTo('body');
-  $('body').append('<p>Copyright - Holberton School</p>');
-
-  $('button').on('click', debounce);
-});
+$('button').on('click', _.debounce(updateCounter, 500));
