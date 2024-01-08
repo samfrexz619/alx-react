65751198f97f120a5e14d439c7d4f1fe19ce943e
samfrexz619 <samfrexz69@gmail.com>
2024-01-08 10:32:14 +0100

    add notification component

--- 0x02-react_props/task_0/dashboard/src/App/App.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.js
@@ -2,14 +2,19 @@ import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Login from "../Login/Login";
+import Notification from '../Notifications/Notifications'
 import './App.css'
+
 
 const App = () => {
   return ( 
     <>
-      <Header />
-      <Login  />
-      <Footer  />
+      <Notification  />
+      <div className="App">
+        <Header />
+        <Login  />
+        <Footer  />
+      </div>
     </>
    );
 }
