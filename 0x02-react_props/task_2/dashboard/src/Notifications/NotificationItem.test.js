a2b09f3c0eb2a47bd38292f4d5c3a32e72a17e00
samfrexz619 <samfrexz69@gmail.com>
2024-01-08 18:07:05 +0100

    add test to component

---
+++ 0x02-react_props/task_2/dashboard/src/Notifications/NotificationItem.test.js
@@ -0,0 +1,26 @@
+/** @jest-environment jsdom */
+import React from 'react'
+import { shallow } from 'enzyme'
+import NotificationItem from './NotificationItem'
+
+
+describe('<NotificationItem  />', () => {
+  it('should render without crashing', () => {
+    const wrapper = shallow(<NotificationItem />)
+    expect(wrapper.exists()).toBe(true)
+  })
+
+  it('should render text and value correctly', () => {
+    const wrapper = shallow(<NotificationItem />)
+    wrapper.setProps({ type: "default", value: "test" });
+    expect(wrapper.html()).toEqual('<li data-notification-type="default" data-priority="default">test</li>');
+  })
+
+  it('should render html', () => {
+    const html = "<u>test</u>"
+    const wrapper = shallow(<NotificationItem html={html} />)
+    const liElement = wrapper.find('li')
+    expect(liElement.prop('dangerouslySetInnerHTML')).toEqual({
+      __html: html})
+  })
+})
--- 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.test.js
+++ 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.test.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import Notifications from './Notifications.js'
+import NotificationItem from './NotificationItem.js'
 
 
 describe('Notification component', ()=> {
@@ -12,7 +13,14 @@ describe('Notification component', ()=> {
 
   it('should render list of items', ()=> {
     const wrapper = shallow(<Notifications  />)
-    expect(wrapper.find('li')).toHaveLength(3)
+    const item = wrapper.find(NotificationItem)
+    expect(item.exists()).toBe(true)
+  })
+
+  it('should render the first item', () => {
+    const wrapper = shallow(<Notifications  />)
+    const firstElem = wrapper.find(NotificationItem).first()
+    expect(firstElem.html()).toBe(`<li data-notification-type="default" data-priority="default">New course available</li>`)
   })
 
   it('should render div with App-header', ()=> {
