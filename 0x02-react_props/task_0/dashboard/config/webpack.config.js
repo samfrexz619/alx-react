782b888cd50820f3c89bc71d3d0bfad82d51a284
samfrexz619 <samfrexz69@gmail.com>
2024-01-08 10:19:27 +0100

    add basic components

---
+++ 0x02-react_props/task_0/dashboard/config/webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+module.exports = {
+  entry: path.resolve(__dirname, '..', './src/index.js'),
+
+  mode: 'development',
+
+  output: {
+    path: path.join(__dirname, '..', 'dist'),
+    filename: 'bundle.js',
+    assetModuleFilename: '[name][ext]'
+  },
+
+  performance: {
+    maxAssetSize: 1000000,
+    maxEntrypointSize: 1000000,
+  },
+
+  devServer: {
+    port: 3000,
+    hot: true,
+    open: true,
+    compress: true,
+  },
+  
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '..', './dist/index.html')
+    }),
+  ],
+
+  module: {
+    rules: [
+      { //babel loader
+        test: /\.(js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader', ],
+      },
+      {
+        test: /\.(?:ico|png|svg|jpeg|jpg|gif)$/i,
+        type: 'asset/resource',
+        use: [
+          // 'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
+    ]
+  }
+}
---
+++ 0x02-react_props/task_0/dashboard/src/App/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Login from "../Login/Login";
+import './App.css'
+
+const App = () => {
+  return ( 
+    <>
+      <Header />
+      <Login  />
+      <Footer  />
+    </>
+   );
+}
+ 
+export default App;
