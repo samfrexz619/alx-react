366b2aa618b375989c0d49cafe98f53e802a5acf
samfrexz619 <samfrexz69@gmail.com>
2024-01-10 16:19:24 +0100

    upd App.js file

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -11,15 +11,15 @@ import './App.css'
 class App extends React.Component {
 
   listCourses = [
-    {id: 1, name: 'ES6', credit: '60'},
-    {id: 2, name: 'Webpack', credit: '20'},
-    {id: 3,name: 'React', credit: '40'},
+    {id: 1, name: 'ES6', credit: '60',},
+    {id: 2, name: 'Webpack', credit: '20',},
+    {id: 3,name: 'React', credit: '40',},
   ];
 
   listNotifications = [
-    { id: 1, type: 'default', value: 'New course available'},
-    {id: 2, type: 'urgent', value: 'New resume available'},
-    {id: 3, type: 'urgent', html: getLatestNotification()},
+    { id: 1, type: 'default', value: 'New course available',},
+    {id: 2, type: 'urgent', value: 'New resume available',},
+    {id: 3, type: 'urgent', html: getLatestNotification(),},
  ];
 
   render() {
