f26226579643cc22fea85ce9c8e3a26596808097
samfrexz619 <samfrexz69@gmail.com>
2024-01-12 15:34:31 +0100

    add BodySection comp

---
+++ 0x03-React_component/task_3/dashboard/src/BodySection/BodySection.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Proptypes from 'prop-types'
+
+
+class BodySection extends React.Component {
+
+  render() {
+    const { title, children } = this.props
+    return(
+      <div className='bodySection'>
+        <h2>{ title }</h2>
+        { children }
+      </div>
+    )
+  }
+}
+
+BodySection.propTypes = {
+  title: Proptypes.string,
+  children: Proptypes.oneOf([Proptypes.string, Proptypes.element])
+}
+export default BodySection;
