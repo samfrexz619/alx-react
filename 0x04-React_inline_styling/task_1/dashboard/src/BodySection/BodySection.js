b7c21cf85b9f20c186a533c8127b928be21913e1
samfrexz619 <samfrexz69@gmail.com>
2024-01-15 15:42:22 +0100

    add dir

---
+++ 0x04-React_inline_styling/task_1/dashboard/src/BodySection/BodySection.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Proptypes from 'prop-types'
+
+
+class BodySection extends React.Component {
+
+  render() {
+    const { title, children } = this.props
+    return(
+      <div className='bodySection'>
+        <h2 className='title'>{ title }</h2>
+        { children }
+      </div>
+    )
+  }
+}
+
+BodySection.propTypes = {
+  title: Proptypes.string,
+  children: Proptypes.oneOfType([Proptypes.string, Proptypes.element])
+}
+export default BodySection;
---
+++ 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseList.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+import CourseListRow from './CourseListRow';
+import { CourseShape } from './CourseShape';
+import './CourseList.css'
+
+const CourseList = ({listCourses}) => {
+  return (
+    <div className='table'>
+      <table id='CourseList'>
+        <thead>
+          <CourseListRow
+            textFirstCell="Available courses"
+            isHeader={true}
+          />
+          <CourseListRow
+            textFirstCell="Course name"
+            textSecondCell="Credit"
+            isHeader={true}
+          />
+        </thead>
+        <tbody>
+          {
+            listCourses.length > 0
+            ? listCourses.map(listCourse => (
+              <CourseListRow
+                key={listCourse.id}
+                textFirstCell={listCourse.name}
+                textSecondCell={listCourse.credit}
+                isHeader={false}
+              />
+            ))
+            : <CourseListRow textFirstCell="No course available yet" />
+          }
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+CourseList.defaultProps = {
+  listCourses: []
+}
+
+CourseList.propTypes = {
+  listCourses: PropTypes.arrayOf(CourseShape)
+}
+
+export default CourseList;
