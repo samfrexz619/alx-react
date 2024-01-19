de8c196e65848882a9ea671b99e5ba86807b20d3
samfrexz619 <samfrexz69@gmail.com>
2024-01-19 11:28:46 +0100

    upd file

--- 0x05-react_state/task_0/dashboard/src/Notifications/Notifications.js
+++ 0x05-react_state/task_0/dashboard/src/Notifications/Notifications.js
@@ -183,7 +183,9 @@ const styles = StyleSheet.create({
 
 Notifications.defaultProps = {
   displayDrawer: true,
-  listNotifications: []
+  listNotifications: [],
+  handleHideDrawer: ()=> {},
+  handleDisplayDrawer: () => {},
 }
 
 Notifications.proptypes = {
