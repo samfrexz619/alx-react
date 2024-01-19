8f198ede529540c4ff01f78a1df0097be7c74692
samfrexz619 <samfrexz69@gmail.com>
2024-01-19 13:21:14 +0100

    add functionality to login comp

--- 0x05-react_state/task_1/dashboard/src/Login/Login.js
+++ 0x05-react_state/task_1/dashboard/src/Login/Login.js
@@ -1,25 +1,83 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, css } from 'aphrodite';
 
 
 const Login = () => {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [enableSubmit, setEnableSubmit] = useState(false)
+
+  const handleLoginSubmit = async(e) => {
+    e.preventDefault();
+    try {
+      setIsLoggedIn(true)
+      const data = {
+        email,
+        password
+      }
+      console.log(data);
+    } catch(err){
+      console.log(err)
+    } finally {
+      // clear input after submitting
+      setEnableSubmit(false)
+      setEmail('')
+      setPassword('')
+    }
+  }
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
+  useEffect(()=> {
+    if(email !== '' && password !== ''){
+      setEnableSubmit(true)
+    } else {
+      if(!enableSubmit){
+        setEnableSubmit(false)
+      }
+    }
+  },[email, password])
+
   return (
-    <div className={css(styles.login)}>
+    <form onSubmit={handleLoginSubmit} className={css(styles.login)}>
         <p>Login to access the full dashboard</p>
       <div className={css(styles.medium)}>
         <div className={css(styles.label)}>
           <label htmlFor="mail">Email:</label>
-          <input type="email" name="mail" id="mail" />
+          <input 
+            type="email" 
+            name="mail" 
+            id="mail" 
+            value={email}
+            onChange={handleChangeEmail}
+          />
         </div>
         <div className={css(styles.label)}>
           <label htmlFor="psswd">Password:</label>
-          <input type="password" name="psswd" id="psswd" />
+          <input 
+            type="password" 
+            name="psswd" 
+            id="psswd" 
+            value={password}
+            onChange={handleChangePassword}
+          />
         </div>
-        <button className={css(styles.btn)}>
-          OK
-        </button>
+        <input 
+          type="submit" 
+          disabled={!enableSubmit} 
+          value={'OK'} 
+          className={css(styles.btn, !enableSubmit && styles.disabledBtn)} 
+        />
       </div>
-    </div>
+    </form>
   );
 }
 
@@ -33,6 +91,9 @@ const styles = StyleSheet.create({
       display: 'flex',
       width: '100%'
     }
+  },
+  disabledBtn: {
+    cursor: 'not-allowed'
   },
   label: {
     width: '100%',
