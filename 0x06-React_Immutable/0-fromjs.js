4778a6fa31ed58d0bfcf4afce80d1899db938716
samfrexz619 <samfrexz69@gmail.com>
2024-01-25 13:52:49 +0100

    upd file

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,12 +1,5 @@
-const { fromJS } = require('immutable');
+import { fromJS } from 'immutable';
 
 export function getImmutableObject (object) {
   return fromJS(object);
 };
-
-// const obj = {
-//   fear: true,
-//   smell: -1033575916.9145899,
-//   wall: false,
-//   thing: -914767132
-// };
