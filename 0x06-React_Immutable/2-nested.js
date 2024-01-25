23bdfe1e37f68947619c4c9cbdc4f5f5101acc9a
samfrexz619 <samfrexz69@gmail.com>
2024-01-25 15:24:58 +0100

    add file

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,4 +1,7 @@
-const {} = require('immutable');
+import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
+  const mappedObj = fromJS(object);
+
+  return mappedObj.getIn(array, undefined);
 }
