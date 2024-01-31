05e2ce324a17ccd4219037601a37bb47aa4b4d70
samfrexz619 <samfrexz69@gmail.com>
2024-01-31 09:50:56 +0100

    add dir

---
+++ 0x05-react_state/task_3/dashboard/src/Header/Header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+import logo from '../assets/holberton-logo.jpg';
+import { AppContext } from '../App/AppContext';
+
+
+class Header extends React.Component {
+  static contextType = AppContext
+
+  render() {
+    const {user, logOut} = this.context
+    console.log(user)
+    return (
+      <>
+        <header className={css(styles.AppHeader)}>
+          <img src={logo} alt="logo" width={300} />
+          <h1>School dashboard</h1>
+        </header>
+        { user.isLoggedIn &&
+          (<section id='logoutSection' style={{color: 'red'}}>
+            <h2>
+              Welcome <strong>{ user.email }</strong>
+              <a onClick={logOut} href="#">
+                <i>(logOut)</i>
+              </a>
+            </h2>
+          </section>)
+        }
+      </>
+    );
+  }
+  
+}
+
+const styles = StyleSheet.create({
+  AppHeader: {
+    display: 'flex',
+    color: '#DF354B',
+    alignItems: 'center',
+    columnGap: '1rem',
+  }
+})
+
+export default Header;
---
+++ 0x05-react_state/task_3/dashboard/src/Login/Login.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+import PropTypes from 'prop-types'
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      //  isLoggedIn: false,
+       email: '',
+       password: '',
+       enableSubmit: false
+    }
+
+    this.handleEmailChange = this.handleChangeEmail.bind(this);
+    this.handlePasswordChange = this.handleChangePassword.bind(this);
+  }
+
+  handleChangeEmail = (e) => {
+    this.setState({
+      email: e.target.value
+    })
+  }
+
+  handleChangePassword = (e) => {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+  handleLoginSubmit = async(e) => {
+    e.preventDefault();
+    const data = {
+      email: this.state.email,
+      password: this.state.password
+    }
+    this.props.logIn(data)
+    console.log(data);
+  }
+
+  componentDidUpdate() {
+    const { email, password, enableSubmit } = this.state;
+
+    if (email !== '' && password !== '') {
+      if (!enableSubmit) {
+        this.setState({ enableSubmit: true });
+      }
+    } else {
+      if (enableSubmit) {
+        this.setState({ enableSubmit: false });
+      }
+    }
+  }
+
+  render() {
+    const {email, password, enableSubmit} = this.state
+    return (
+      <form onSubmit={this.handleLoginSubmit} className={css(styles.login)}>
+          <p>Login to access the full dashboard</p>
+        <div className={css(styles.medium)}>
+          <div className={css(styles.label)}>
+            <label htmlFor="mail">Email:</label>
+            <input 
+              type="email" 
+              name="mail" 
+              id="mail" 
+              value={email}
+              onChange={this.handleChangeEmail}
+            />
+          </div>
+          <div className={css(styles.label)}>
+            <label htmlFor="psswd">Password:</label>
+            <input 
+              type="password" 
+              name="psswd" 
+              id="psswd" 
+              value={password}
+              onChange={this.handleChangePassword}
+            />
+          </div>
+          <input 
+            type="submit" 
+            disabled={!enableSubmit} 
+            value={'OK'} 
+            className={css(styles.btn, !enableSubmit && styles.disabledBtn)} 
+          />
+        </div>
+      </form>
+    );
+  }
+  
+}
+
+
+const styles = StyleSheet.create({
+  login: {
+    padding: '20px 0',
+    width: '100%'
+  },
+  medium: {
+    '@media (min-width: 900px)': {
+      display: 'flex',
+      width: '100%'
+    }
+  },
+  disabledBtn: {
+    cursor: 'not-allowed'
+  },
+  label: {
+    width: '100%',
+    display: 'flex',
+    columnGap: '10px',
+    margin: '10px 5px',
+    '@media (min-width: 900px)': {
+      width: 'auto',
+      margin: '0 5px'
+    }
+  },
+  btn: {
+    cursor: 'pointer',
+    marginLeft: '7px',
+    '@media (min-width: 900px)': {
+      marginLeft: 0,
+    }
+  }
+})
+
+Login.propTypes = {
+  logIn: PropTypes.func
+}
+export default Login;
