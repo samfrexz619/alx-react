fbec6339f8902dcf4a464ae5403ed064c824b9a6
samfrexz619 <samfrexz69@gmail.com>
2024-01-31 20:12:33 +0100

    add dir

---
+++ 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StyleSheet, css } from 'aphrodite';
+import BodySection from './BodySection';
+
+
+class BodySectionWithMarginBottom extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div className={css(styles.bodySectionWithMargin)}>
+        <BodySection {...this.props} />
+      </div>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  bodySectionWithMargin: {
+    marginBottom: '40px'
+  }
+})
+
+BodySectionWithMarginBottom.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+};
+
+export default BodySectionWithMarginBottom;
---
+++ 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/actions/uiActionCreators.test.js
@@ -0,0 +1,33 @@
+/** @jest-environment jsdom */
+import React from 'react'
+import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER } from "./uiActionTypes";
+
+import { login, logout, hideNotificationDrawer, displayNotificationDrawer } from "./uiActionCreators";
+
+describe("tests for UI notification action creators", () => {
+  it("should create proper action for login", () => {
+    const email = "frexz@gmail.com";
+    const password = "12345";
+
+    expect(login(email, password)).toEqual({
+      type: LOGIN,
+      user: { email: "frexz@gmail.com", password: "12345" },
+    });
+  });
+
+  it("should create proper action for logout", () => {
+    expect(logout()).toEqual({ type: LOGOUT });
+  });
+
+  it("should create proper action for displaying notification drawer", () => {
+    expect(displayNotificationDrawer()).toEqual({
+      type: DISPLAY_NOTIFICATION_DRAWER,
+    });
+  });
+
+  it("should create proper action for hiding notification drawer", () => {
+    expect(hideNotificationDrawer()).toEqual({
+      type: HIDE_NOTIFICATION_DRAWER,
+    });
+  });
+});
